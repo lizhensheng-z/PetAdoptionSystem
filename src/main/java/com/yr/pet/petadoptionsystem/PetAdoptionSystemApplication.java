3462f0eafa3b82edeeb3583473b62bca6fef9efa
宗平 <zhensheng.lzs@digital-engine.com>
2026-01-28 21:58:59 +0800

    Initial commit

---
+++ src/main/java/com/yr/pet/petadoptionsystem/PetAdoptionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yr.pet.petadoptionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetAdoptionSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yr/pet/petadoptionsystem/PetAdoptionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yr.pet.petadoptionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
