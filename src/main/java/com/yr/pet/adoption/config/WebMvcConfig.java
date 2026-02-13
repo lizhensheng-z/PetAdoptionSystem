7e31a245995d2aa9ca274486b71dc614e8e2f82f
李振生 <2734569998@qq.com>
2026-02-13 11:35:36 +0800

    fix:拦截器配置

--- src/main/java/com/yr/pet/adoption/config/WebMvcConfig.java
+++ src/main/java/com/yr/pet/adoption/config/WebMvcConfig.java
@@ -12,18 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    /**
-     * 配置跨域
-     */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
 
     /**
      * 配置RestTemplate
