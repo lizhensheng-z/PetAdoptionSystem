a63193ac87e114ae7a940124c6317bcf271d17d2
宗平 <zhensheng.lzs@digital-engine.com>
2026-04-05 15:47:50 +0800

    fix：系统公告，用户删除

--- src/main/java/com/yr/pet/adoption/model/dto/BatchDeleteUsersRequest.java
+++ src/main/java/com/yr/pet/adoption/model/dto/BatchDeleteUsersRequest.java
@@ -1,5 +1,6 @@
 package com.yr.pet.adoption.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
@@ -16,5 +17,6 @@ public class BatchDeleteUsersRequest {
 
     @Schema(description = "用户ID列表", required = true)
     @NotEmpty(message = "用户ID列表不能为空")
+    @JsonProperty(value = "ids")
     private List<Long> userIds;
 }
